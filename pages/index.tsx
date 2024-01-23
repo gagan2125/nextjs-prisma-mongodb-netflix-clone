8460127735b1d34e870915fcd70b4da93d5a0430
gagan2125 <gagansk2125@gmail.com>
2024-01-23 17:28:03 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+    </>
+    
+  );
+}
