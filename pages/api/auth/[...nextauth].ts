3088ef6628f5e9d853b6960416e783aefa1ce865
gagan2125 <108995377+gagan2125@users.noreply.github.com>
2024-01-31 21:22:03 +0530

    eleventh commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,10 +13,6 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
     Credentials({
       id: 'credentials',
       name: 'Credentials',
